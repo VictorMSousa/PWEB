26403de7ad35c5c68d1642d3614f1f80d0da0c56
StampCZ <76538748+VictorMSousa@users.noreply.github.com>
2022-05-16 09:43:24 -0300

    atividade10
    atividade10

---
+++ Atividade10/script.js
@@ -0,0 +1,21 @@
+let Aluno = newObject();
+Aluno.ra = 10101010;
+Aluno.nome = "Aleatorio1";
+
+console.log(Aluno.nome)
+
+let Aluno2 = {
+    ra2:111,
+    nome2: "Aleatorio2"
+}
+
+let Aluno3 = {};
+let nome3,ra3;
+Aluno3[nome3] = "Aleatorio3";
+Aluno3[ra3] = 222;
+
+let Aluno4 = {}
+Aluno4["nome4"] = "Aleatorio4";
+Aluno4["ra4"] = 1234;
+
+
