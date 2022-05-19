9be1619beb69ed3b0fb8f184cd0822e7abc56132
StampCZ <76538748+VictorMSousa@users.noreply.github.com>
2022-05-18 21:37:36 -0300

    Atividade12

---
+++ Atividade12/script.js
@@ -0,0 +1,16 @@
+
+
+
+function abrir(){
+    document.getElementById("Janela").src="Imagens/janelaaberta.png";
+    document.getElementById("Paragrafo").innerText = "Janela Aberta";
+}
+function fechar(){
+    document.getElementById("Janela").src="Imagens/janelafechada.png"
+    document.getElementById("Paragrafo").innerText = "Janela Fechada";
+}
+function quebrar(){
+    document.getElementById("Janela").src="Imagens/janelaquebra.png"
+    document.getElementById("Paragrafo").innerText = "Janela Quebrada";
+}
+
